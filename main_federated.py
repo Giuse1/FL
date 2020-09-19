b48b18b402a660c2f5ea7dc9d61c143ca9ec76e6
Giuse1 <48623997+Giuse1@users.noreply.github.com>
2020-09-19 22:24:31 +0200

    modified validation set

--- main_federated.py
+++ main_federated.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from __future__ import division
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -8,48 +9,56 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision
 from torchvision import datasets, models, transforms
-
 from model import CNNMnist
 from FL.FL_train import train_model
-
-print("PyTorch Version: ", torch.__version__)
-print("Torchvision Version: ", torchvision.__version__)
+print("PyTorch Version: ",torch.__version__)
+print("Torchvision Version: ",torchvision.__version__)
 torch.manual_seed(0)
 
-FILE_NAME = 'acc_custom_f_50e.txt'
-BATCH_SIZE = 8
-NUM_ROUNDS = 50
-LOCAL_EPOCHS = 1
-NUM_CLASSES = 10
-MODEL_NAME = "CNNmnist"
+
+batch_size = 8
+num_rounds = 50
+local_epochs = 1
+
+model_name = "resnet"
+num_classes = 10
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-def initialize_model(model_name, num_classes=10, num_channels=1):
-    if model_name == "resnet":
-        model= models.resnet18(pretrained=use_pretrained)
-        num_ftrs = model.fc.in_features
-        model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+def initialize_model(num_classes=10, num_channels=1):
 
-        model.fc = nn.Linear(num_ftrs, num_classes)
-        # input_size = 28
+    # model_ft = models.resnet18(pretrained=use_pretrained)
+    # num_ftrs = model_ft.fc.in_features
+    # model_ft.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3,bias=False)
+    #
+    # model_ft.fc = nn.Linear(num_ftrs, num_classes)
+    # input_size = 28
 
-    else:
-        model = CNNMnist(num_channels=num_channels, num_classes=num_classes)
+    model_ft = CNNMnist(num_channels=num_channels, num_classes=num_classes)
 
-    return model
+    return model_ft
 
-
-model_ft = initialize_model(MODEL_NAME, NUM_CLASSES, num_channels=1)
+model_ft = initialize_model(num_classes, num_channels=1)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 model_ft = model_ft.to(device)
+
+#transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,)),])
+#trainset = datasets.MNIST('', download=True, train=True, transform=transform)
+#valset = datasets.MNIST('', download=True, train=False, transform=transform)
+
+#trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+#valloader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)
+#dataloaders_dict = {'train': trainloader, 'val': valloader }
 
 optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.1, momentum=0.9)
 criterion = nn.CrossEntropyLoss()
 
-hist_acc = train_model(model_ft, criterion, num_rounds=NUM_ROUNDS, local_epochs=LOCAL_EPOCHS)
+hist = train_model(model_ft, criterion, num_rounds=num_rounds, local_epochs=local_epochs)
 
 hist_acc = [x.cpu().numpy() for x in hist]
+hist_acc
+
+
 plt.plot(hist_acc)
-plt.show()
-hist_acc_arr = np.array(hist_acc)
-np.savetxt(FILE_NAME, hist_acc_arr)
+arr = np.array(hist_acc)
+np.savetxt('acc_custom_f_50e.txt', arr)

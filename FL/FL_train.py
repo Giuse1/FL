f0286586ba91ca715d45057298d7ffb88ed79eeb
Giuse1 <48623997+Giuse1@users.noreply.github.com>
2020-10-02 12:26:54 +0200

    modified validation se

--- FL/FL_train.py
+++ FL/FL_train.py
@@ -101,7 +101,7 @@ def average_weights(w, samples_per_client):
     """
     Returns the average of the weights.
     """
-    w_avg = copy.deepcopy(w[0])
+    w_avg = torch.true_divide(w[0], 1/samples_per_client[0])
     for key in w_avg.keys():
         for i in range(1, len(w)):
             w_avg[key] += torch.true_divide(w[i][key], 1/samples_per_client[i])

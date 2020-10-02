b755b9eb3280c7b773fe83777dadf99b8a34a54b
Giuse1 <48623997+Giuse1@users.noreply.github.com>
2020-10-02 11:11:16 +0200

    modified validation se

--- FL/FL_train.py
+++ FL/FL_train.py
@@ -18,11 +18,11 @@ def train_model(global_model, criterion, num_rounds, local_epochs, num_users, ba
     total_num_users = len(list_users)
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,)), ])
 
-    NIID_trainloader_list= getDataloaderNIIDList(path='data/', total_num_users=num_users,transform=transform, batch_size=batch_size, shuffle=True)
-    #trainloader_list = getDataloaderList(path='data/', transform=transform, batch_size=batch_size, shuffle=True)
+    #NIID_trainloader_list= getDataloaderNIIDList(path='data/', total_num_users=num_users,transform=transform, batch_size=batch_size, shuffle=True)
+    trainloader_list = getDataloaderList(path='data/', transform=transform, batch_size=batch_size, shuffle=True)
     valloader_list = getDataloaderList(path='data_test/', transform=transform, batch_size=batch_size, shuffle=True)
 
-    random_list = range(num_users)
+    #random_list = range(num_users)
 
     for round in range(num_rounds):
         print('-' * 10)
@@ -34,25 +34,30 @@ def train_model(global_model, criterion, num_rounds, local_epochs, num_users, ba
                 gloabl_num_correct = 0
                 global_num_total = 0
                 global_loss = 0
+                samples_per_client = []
+
+                random_list = random.sample(range(total_num_users),num_users)
 
                 for idx in random_list:
-                    local_model = LocalUpdate(dataloader=NIID_trainloader_list[idx], id=idx, criterion=criterion,
+                    local_model = LocalUpdate(dataloader=trainloader_list[idx], id=idx, criterion=criterion,
                                                local_epochs=local_epochs, learning_rate=learning_rate)
                     w, local_loss, local_correct, local_total = local_model.update_weights(
                         model=copy.deepcopy(global_model).double())
                     local_weights.append(copy.deepcopy(w))
-                    gloabl_num_correct += local_correct
-                    global_num_total += local_total
-                    global_loss += local_loss
+                    samples_per_client.append(local_total)
+
+                    #gloabl_num_correct += local_correct
+                    #global_num_total += local_total
+                    #global_loss += local_loss
 
 
-                global_weights = average_weights(local_weights)
+                global_weights = average_weights(local_weights, samples_per_client)
                 global_model.load_state_dict(global_weights)
 
-                train_loss.append(global_loss / global_num_total)
-                train_acc.append(gloabl_num_correct / global_num_total)
+                #train_loss.append(global_loss / global_num_total)
+                #train_acc.append(gloabl_num_correct / global_num_total)
 
-                print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, global_loss/global_num_total, gloabl_num_correct/global_num_total ))
+                #print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, global_loss/global_num_total, gloabl_num_correct/global_num_total ))
 
 
             else:
@@ -95,14 +100,14 @@ def model_evaluation(model, dataloader_list, criterion):
         return epoch_loss, epoch_acc
 
 
-def average_weights(w):
+def average_weights(w, samples_per_client):
     """
     Returns the average of the weights.
     """
     w_avg = copy.deepcopy(w[0])
     for key in w_avg.keys():
         for i in range(1, len(w)):
-            w_avg[key] += w[i][key]
+            w_avg[key] += torch.true_divide(w[i][key], 1/samples_per_client[i])
         w_avg[key] = torch.true_divide(w_avg[key], len(w))
     return w_avg
 

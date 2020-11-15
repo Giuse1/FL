c8e2a36dfe92ab480341b8b7ffdb1994cdc03dab
Giuse1 <48623997+Giuse1@users.noreply.github.com>
2020-11-15 13:48:29 +0100

    modified validation se

--- FL/FL_user.py
+++ FL/FL_user.py
@@ -18,7 +18,7 @@ class LocalUpdate(object):
             local_correct = 0
             local_loss = 0.0
             for (i, data) in enumerate(self.dataloader):
-                images, labels = data[0].to(self.device), data[1].to(self.device)
+                images, labels = data['input'].to(self.device), data['label'].to(self.device)
 
                 optimizer.zero_grad()
                 log_probs = model(images.double())
